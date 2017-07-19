19cdc04962c1db6c010ce52e83a792be3cda710a
Elijah Zupancic <elijah@zupancic.name>
2017-07-19 11:58:45 -0700

    Replaced error handling logic.

--- src/github.com/dekobon/clamav-mirror/clamav-mirror.go
+++ src/github.com/dekobon/clamav-mirror/clamav-mirror.go
@@ -5,22 +5,40 @@ import (
 	"os"
 	"net"
 	"github.com/pborman/getopt"
+	"github.com/hashicorp/errwrap"
 	"path/filepath"
 	"fmt"
 	"strings"
+	"errors"
 );
 
-func main() {
-	logger := log.New(os.Stdout, "", log.LstdFlags);
-	verboseMode := parseCliFlags();
+var logger *log.Logger;
+var logFatal *log.Logger;
 
-	sigtoolPath := findSigtoolPath();
+func init() {
+	logger = log.New(os.Stdout, "", log.LstdFlags);
+	logFatal = log.New(os.Stderr, "", log.LstdFlags);
+}
+
+func main() {
+	verboseMode, dataFilePath := parseCliFlags();
+
+	sigtoolPath, err := findSigtoolPath();
+
+	if (err != nil) {
+		logFatal.Fatal(err);
+	}
+
 	if (verboseMode) {
 		logger.Printf("ClamAV executable sigtool found at path: %v", sigtoolPath);
 	}
 
 	var mirrorDomain string = "current.cvd.clamav.net";
-	var mirrorTxtRecord string = pullTxtRecord(mirrorDomain);
+	mirrorTxtRecord, err := pullTxtRecord(mirrorDomain);
+
+	if (err != nil) {
+		logFatal.Fatal(err);
+	}
 
 	if (verboseMode) {
 		logger.Printf("TXT record for [%v]: %v", mirrorDomain, mirrorTxtRecord);
@@ -33,36 +51,44 @@ func main() {
 			"[clamav=%v,mainv=%v,dailyv=%v,x=%v,y=%v,z=%v,safebrowsingv=%v,bytecodev=%v",
 			clamav, mainv, dailyv, x, y, z, safebrowsingv, bytecodev);
 	}
+
+	updateFile(dataFilePath);
 }
 
-func parseCliFlags() (bool) {
+func parseCliFlags() (bool, string) {
 	verbosePart := getopt.BoolLong("verbose", 'v', "Enable verbose mode with additional debugging information");
+	dataFilePart := getopt.StringLong("data-file-path", 'd', "/var/clamav/data", "Path to ClamAV data files");
 	getopt.Parse();
 
-	var verboseMode bool = *verbosePart;
+	if (!exists(*dataFilePart)) {
+		msg := fmt.Sprintf("Data file path doesn't exist or isn't accessible: %v", *dataFilePart);
+		logFatal.Fatal(msg);
+	}
 
-	return verboseMode;
+	dataFileAbsPath, err := filepath.Abs(*dataFilePart);
+
+	if (err != nil) {
+		msg := fmt.Sprintf("Unable to parse absolute path of data file path: %v", *dataFilePart);
+		logFatal.Fatal(msg);
+	}
+
+	return *verbosePart, dataFileAbsPath;
 }
 
-func help() {
-	getopt.Usage();
-	os.Exit(0);
-}
-
-func pullTxtRecord(mirrorDomain string) (string) {
+func pullTxtRecord(mirrorDomain string) (string, error) {
 	mirrorTxtRecords, err := net.LookupTXT(mirrorDomain);
 
 	if (err != nil) {
-		msg := fmt.Sprintf("Unable to resolve TXT record for %v", mirrorDomain);
-		log.Fatal(msg, err);
+		msg := fmt.Sprintf("Unable to resolve TXT record for %v. {{err}}", mirrorDomain);
+		return "", errwrap.Wrapf(msg, err);
 	}
 
 	if (len(mirrorTxtRecords) < 1) {
-		msg := fmt.Sprintf("No TXT records returned for %v", mirrorDomain);
-		log.Fatal(msg);
+		msg := fmt.Sprintf("No TXT records returned for %v. {{err}}", mirrorDomain);
+		return "", errwrap.Wrapf(msg, err);
 	}
 
-	return mirrorTxtRecords[0];
+	return mirrorTxtRecords[0], nil;
 }
 
 func parseTxtRecord(mirrorTxtRecord string) (string, string, string, string, string, string, string, string) {
@@ -70,11 +96,7 @@ func parseTxtRecord(mirrorTxtRecord string) (string, string, string, string, str
 	return s[0], s[1], s[2], s[3], s[4], s[5], s[6], s[7];
 }
 
-func updateFile(file string) {
-
-}
-
-func findSigtoolPath() (path string) {
+func findSigtoolPath() (string, error) {
 	var execName string = "sigtool";
 	var separator string = string(os.PathSeparator);
 	var envPathSeparator string = string(os.PathListSeparator);
@@ -85,24 +107,24 @@ func findSigtoolPath() (path string) {
 		execPath, err := filepath.Abs(localPath);
 
 		if (err != nil) {
-			msg := fmt.Sprintf("Error parsing absolute path for: %v", localPath);
-			log.Fatal(msg, err);
+			msg := fmt.Sprintf("Error parsing absolute path for: %v. {{err}}", localPath);
+			return "", errwrap.Wrapf(msg, err);
 		}
 
-		return execPath;
+		return execPath, nil;
 	}
 
 	for _, pathElement := range strings.Split(envPath, envPathSeparator) {
 		var execPath string = pathElement + separator + execName;
 		if (exists(execPath)) {
-			return execPath;
+			return execPath, nil;
 		}
 	}
 
-	log.Fatal("The ClamAV executable sigtool was not found in the " +
-		"current directory nor in the system path");
+	err := errors.New("The ClamAV executable sigtool was not found in the " +
+		"current directory nor in the system path.");
 
-	return;
+	return "", err;
 }
 
 func exists(filePath string) (exists bool) {
@@ -113,4 +135,8 @@ func exists(filePath string) (exists bool) {
 	}
 
 	return;
+}
+
+func updateFile(file string) {
+
 }
