c0969f80832b5a739ea7e3cd465b2ad4e697c969
Elijah Zupancic <elijah@zupancic.name>
2017-07-27 16:55:41 -0700

    Added additional time utils test

--- src/github.com/dekobon/clamav-mirror/utils/time_utils_test.go
+++ src/github.com/dekobon/clamav-mirror/utils/time_utils_test.go
@@ -5,7 +5,7 @@ import (
 	"time"
 )
 
-func TestParseClamAVTimeStamp(t *testing.T) {
+func TestValidParseClamAVTimeStamp(t *testing.T) {
 	expected, err := time.Parse(time.RFC3339, "2006-01-02T15:04:00-07:00")
 
 	if err != nil {
@@ -23,5 +23,13 @@ func TestParseClamAVTimeStamp(t *testing.T) {
 			"Expected: %v\n"+
 			"Actual  : %v",
 			expected, parsed)
+	}
+}
+
+func TestInvalidParseClamAVTimeStamp(t *testing.T) {
+	_, err := ParseClamAVTimeStamp("02 Jan 2006 15:04Z")
+
+	if err == nil {
+		t.Error("Expected time parsing exception not thrown")
 	}
 }
